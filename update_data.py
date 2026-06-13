c83fcab8424d2b769d9b427f1fcbe86e7fcc9677
nike1112afmax <nike1112@gmail.com>
2026-06-14 01:29:44 +0800

    Update update_data.py

--- update_data.py
+++ update_data.py
@@ -39,7 +39,6 @@ FRED_ECON = {
     'us_unemployment':'UNRATE',
     'us_gdp':        'GDPC1',
     'us_michigan':   'UMCSENT',
-    'us_confboard':  'CSCICP03USM665S',
     'eu_cpi':        'CP0000EZ19M086NEST',
     'tw_cpi':        'TWNPCPIPCPPPT',
 }
